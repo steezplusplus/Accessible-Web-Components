42f2adb9f5ed81af27f63a2a6116b8a322e62220
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-13 12:21:27 -0400

    feat: mv lorem, create Article component

---
+++ components/article/Article.tsx
@@ -0,0 +1,12 @@
+
+export function Article() {
+  return (
+    <article className="border border-solid">
+      <h2>Title</h2>
+      <span>Tag 1</span>
+      <span>Tag 2</span>
+      <span>Tag 3</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id risus gravida, dictum lorem</p>
+    </article>
+  )
+}
