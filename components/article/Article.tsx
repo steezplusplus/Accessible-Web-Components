94a6d37f204cd981256f2b88cbe93837ebfd7607
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-13 15:53:52 -0400

    style: article init styles

--- components/article/Article.tsx
+++ components/article/Article.tsx
@@ -1,11 +1,21 @@
 
 export function Article() {
   return (
-    <article className="border border-solid">
-      <h2>Title</h2>
-      <span>Tag 1</span>
-      <span>Tag 2</span>
-      <span>Tag 3</span>
+    <article className="border border-solid px-1">
+      <h2 className="text-lg">Title</h2>
+      <div>
+        <ul className="flex gap-x-2">
+          <li className="border border-solid rounded px-1">
+            <span className="text-sm">Tag 1</span>
+          </li>
+          <li className="border border-solid rounded px-1">
+            <span className="text-sm">Tag 2</span>
+          </li>
+          <li className="border border-solid rounded px-1">
+            <span className="text-sm">Tag 3</span>
+          </li>
+        </ul>
+      </div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id risus gravida, dictum lorem</p>
     </article>
   )
