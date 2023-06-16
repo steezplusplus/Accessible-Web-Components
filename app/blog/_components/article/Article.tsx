81f41b4b3216a92c1a96f4cc21cedecc73827b8c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-16 19:56:27 -0400

    feat: add link to article

--- app/blog/_components/article/Article.tsx
+++ app/blog/_components/article/Article.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Tag } from "../tag/Tag";
 
 export type ArticleProps = {
@@ -13,7 +14,7 @@ export function Article(props: ArticleProps) {
   const { title, tags, summary }  = props;
   return (
     <article className="border border-solid border-black rounded-sm p-1">
-      <h2 className="text-lg">{title}</h2>
+      <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="#">{title}</Link>
       <div>
         <ul className="flex gap-x-2">
           {tags.map((tag, i) => {
