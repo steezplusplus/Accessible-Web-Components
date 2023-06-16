44ce8bd329ca0cea480f89c928c9fd92b5830fdf
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-16 15:46:12 -0400

    feat: init new home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <>
-      <p>Homepage</p>
+      <section className="flex flex-col">
+        <h1 className="text-4xl">Helping people make the web a better place through accessible software.</h1>
+        <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
+        <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
+      </section>
     </>
   );
 }
