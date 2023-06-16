08f073936c840db96b6a31e188f9663aa8959a67
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-16 14:36:38 -0400

    feat: get blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 import { Article } from "@/app/_components/article/Article";
 
-export default function Home() {
+const BUTTERCMS_TOKEN = process.env.NEXT_PUBLIC_BUTTERCMS_TOKEN || '';
+
+async function getBlogPosts() {
+  const res = await fetch(`https://api.buttercms.com/v2/posts/?auth_token=${BUTTERCMS_TOKEN}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const blogPosts = await getBlogPosts();
+
   return (
     <>
+    {blogPosts.data.map((blogPost: any, i: number) => {
+      const { title } = blogPost;
+      return <p key={`${title}-${i}`}>{title}</p>
+    })}
       <Article />
       <Article />
       <Article />
