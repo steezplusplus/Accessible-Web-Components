8b242f47ca07f23d08ac28fa6a0e951190692fd4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-18 17:36:50 -0400

    feat: Get blog post and display it

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,25 @@
-export default function BlogPost({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>
+import { BlogPost, getBlogPosts } from "../_api/api";
+
+export type BlogPostParams = { 
+  params: { slug: string } 
+};
+
+export default async function BlogPost({ params }: BlogPostParams ) {
+  const blogPosts: BlogPost = await getBlogPosts();
+  const { data: blogPostsData } = blogPosts;
+  const blogPost = blogPostsData.find((blogPosts) => blogPosts.slug === params.slug );
+
+  if (!blogPost) {
+    return (
+      <div>No blog post found.</div>
+    );
+  }
+
+  const { title, body } = blogPost;
+  return (
+    <div>
+      <h1 className="text-4xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: body }}></div>
+    </div>
+  );
 }
