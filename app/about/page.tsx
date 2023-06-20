dd154c6faea515ec4c812b151a5257179dac5221
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-20 13:58:10 -0400

    fix: fix lint error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <>
-      <h1 className="text-4xl">Hi, I'm Jesse. I'm a ful time software engineer.</h1>
+      <h1 className="text-4xl">Hi, I&apos;m Jesse. I&apos;m a ful time software engineer.</h1>
       <h2>I make the web a better place through building websites following W3C Web Acessibility Initiative standards.</h2>
     </>
   );
