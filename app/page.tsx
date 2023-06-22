c14224713e9c11915ed1477557896fa98a12c53a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-21 23:15:40 -0400

    feat: links inline

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,19 @@ export default async function Home() {
   return (
     <div>
       <header className="grid sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-        <div className="col-span-full flex items-center justify-center border-4 border-solid border-red-800 lg:col-span-7 lg:col-start-6">
+        <div className="col-span-full flex items-center justify-center lg:col-span-7 lg:col-start-6">
           Asset
         </div>
-        <div className="col-span-full border-4 border-solid border-green-800 lg:col-span-5 lg:col-start-1 lg:row-start-1">
+        <div className="col-span-full lg:col-span-5 lg:col-start-1 lg:row-start-1">
           <h2 className="text-4xl">Helping people make the web a better place through accessible software.</h2>
-          <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
-          <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
+          <ul>
+            <li>
+              <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
+            </li>
+            <li>
+              <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
+            </li>
+          </ul>
         </div>
       </header>
     </div>
