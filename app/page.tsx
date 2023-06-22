e157a9e71ec21f67f30592ad3f3e0a5f3cfbc9ce
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-21 23:07:56 -0400

    feat: simplify grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <header className="mx-auto grid h-auto max-w-7xl grid-cols-4 gap-x-4 border border-dotted border-red-500 pt-12 md:grid-cols-8 lg:min-h-[40rem] lg:grid-cols-12 lg:gap-x-6 lg:pb-12">
-        <div className="col-span-full mb-12 flex items-center justify-center border border-solid lg:col-span-7 lg:col-start-6 lg:-mt-12 lg:mb-0 lg:px-0">
+    <div>
+      <header className="grid sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
+        <div className="col-span-full flex items-center justify-center border-4 border-solid border-red-800 lg:col-span-7 lg:col-start-6">
           Asset
         </div>
-        <div className="col-span-full flex flex-col border border-solid pt-6 lg:col-span-5 lg:col-start-1 lg:row-start-1 lg:h-full">
+        <div className="col-span-full border-4 border-solid border-green-800 lg:col-span-5 lg:col-start-1 lg:row-start-1">
           <h2 className="text-4xl">Helping people make the web a better place through accessible software.</h2>
           <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
           <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
