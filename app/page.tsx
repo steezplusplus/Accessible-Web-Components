9b3580d9307bb7376dde0f783ad89936bb321f37
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-22 23:07:51 -0400

    feat: reduce homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,18 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <div>
-      <header className="grid sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-        <div className="col-span-full flex items-center justify-center lg:col-span-7 lg:col-start-6">
-          Asset
-        </div>
-        <div className="col-span-full lg:col-span-5 lg:col-start-1 lg:row-start-1">
-          <h2 className="text-4xl">Helping people make the web a better place through accessible software.</h2>
-          <ul>
-            <li>
-              <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
-            </li>
-            <li>
-              <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
-            </li>
-          </ul>
-        </div>
+      <header>
+        <ul>
+          <li>
+            <h2 className="text-4xl">Helping people make the web a better place through accessible software.</h2>
+          </li>
+          <li>
+            <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Read the blog</Link>
+          </li>
+          <li>
+            <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href="/about">Learn about me</Link>
+          </li>
+        </ul>
       </header>
     </div>
   );
