d97623b618e999d605c84e1787143add0d00771c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 15:16:12 -0400

    feat: remove butter, rebuild article

--- app/blog/_components/article/Article.tsx
+++ app/blog/_components/article/Article.tsx
@@ -1,32 +1,18 @@
-import Link from 'next/link';
-import { Tag } from "../tag/Tag";
-
-export type ArticleProps = {
-  title: string;
-  slug: string;
-  tags: {
-    name: string;
-    slug: string;
-  }[];
-  summary: string;
-}
-
-export function Article(props: ArticleProps) {
-  const { title, slug, tags, summary } = props;
-  console.log(slug);
+export function Article() {
   return (
-    <article className="border border-solid border-black rounded-sm p-1">
-      <Link className="text-xl text-blue-800 hover:text-blue-900 hover:underline" href={`/blog/${slug}`}>{title}</Link>
-      <div>
-        <ul className="flex gap-x-2">
-          {tags.map((tag, i) => {
-            return <li key={`${tag.name}-${i}`}>
-              <Tag displayText={tag.name} />
-            </li>;
-          })}
-        </ul>
+    <article className="mb-5 max-w-sm rounded-lg border border-gray-200 bg-white shadow-md">
+      <a href="#">
+        <div className="h-48 w-full bg-amber-500" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Title</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700">Description</p>
+        <a className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300" href="#">
+          Read more
+        </a>
       </div>
-      <p>{summary}</p>
     </article>
   );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,31 @@
+import { Article } from "./_components/article/Article";
+
 export default async function Blog() {
   return (
     <div>
       <header className="bg-gradient-to-r from-amber-500 to-pink-500">
         <ul className="flex flex-col gap-y-8 p-8">
           <li>
-            <h1 className="text-7xl leading-tight text-white">Learn a11y and frontend dev with these articles</h1>
+            <h2 className="text-7xl leading-tight text-white">Blog</h2>
+          </li>
+          <li>
+            <h3 className="text-white">Learn a11y and frontend dev with these articles</h3>
           </li>
         </ul>
       </header>
+
+      <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </section>
+
     </div>
   );
 }
