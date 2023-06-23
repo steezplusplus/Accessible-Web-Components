d688d654d7b0e352d922f125d820fb4f9ba6ae79
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 14:49:11 -0400

    feat: remove buttercms

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,4 @@
-import { Article } from "./_components/article/Article";
-import { BlogPost, getBlogPosts } from "./_api/api";
-
 export default async function Blog() {
-  const blogPosts: BlogPost = await getBlogPosts();
-  const { data: blogPostsData } = blogPosts;
-
-  const tagNames = blogPostsData.map((blogPost) => blogPost.tags.map((tag) => tag.name)).flat();
-  const uniqueTagNames = [...new Set(tagNames)];
-
   return (
     <div>
       <header className="bg-gradient-to-r from-amber-500 to-pink-500">
@@ -17,27 +8,6 @@ export default async function Blog() {
           </li>
         </ul>
       </header>
-
-      <section className="container m-auto">
-        <fieldset className="flex gap-x-8">
-          <legend>Search by tags:</legend>
-          {uniqueTagNames.map((tagName, i) => {
-            return (
-              <label key={`${tagName}-${i}`}>
-                <span className="mr-1">{tagName}</span>
-                <input type="checkbox" />
-              </label>
-            );
-          })}
-        </fieldset>
-      </section>
-
-      <section className="container m-auto grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {blogPostsData.map((blogPost, i: number) => {
-          const { title, tags, summary, slug } = blogPost;
-          return <Article key={`${title}-${i}`} title={title} slug={slug} tags={tags} summary={summary} />;
-        })}
-      </section>
     </div>
   );
 }
