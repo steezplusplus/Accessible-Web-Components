0156c5d0003d83376eb70198135dbd082d2443be
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 01:00:28 -0400

    feat: wooooo

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,31 @@ export default async function Home() {
   return (
     <div>
       <header className="bg-gradient-to-r from-emerald-500 to-emerald-900">
-        <ul className="p-8">
+        <ul className="flex flex-col gap-y-8 p-8">
           <li>
             <h2 className="text-7xl text-white">Making the web a better place through accessible software.</h2>
           </li>
           <li>
-            <Link className="text-xl text-white hover:underline" href="/blog">Read the blog</Link>
-          </li>
-          <li>
-            <Link className="text-xl text-white hover:underline" href="/about">Learn about me</Link>
+            <ul className="flex flex-row items-center gap-x-8">
+              <li className="rounded border border-white px-4 py-2">
+                <Link className="text-xl font-light text-white hover:underline" href="/blog">
+                  <span className="flex">
+                    Read the blog
+                    <svg className="ml-2 w-5" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                  </span>
+                </Link>
+              </li>
+              <li className="flex flex-row items-center gap-x-8 rounded border border-white px-4 py-2">
+                <span className="flex">
+                  <Link className="text-xl text-white hover:underline" href="/about">More about me</Link>
+                  <svg className="ml-2 w-5" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                  </svg>
+                </span>
+              </li>
+            </ul>
           </li>
         </ul>
       </header>
