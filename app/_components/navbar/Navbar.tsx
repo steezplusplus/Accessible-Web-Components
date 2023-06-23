a523c5bf73290ae733b928adb00d08bd4d4b4400
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-22 23:35:30 -0400

    feat: add some brand styling to the navbar

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -2,18 +2,27 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <ul className="flex h-full items-center border border-solid px-1">
+    <ul className="flex h-full items-center bg-blue-500 px-5">
       <li>
-        <Link href="/">
-          <h1 className="text-2xl hover:underline">Steez++</h1>
+        <Link className="group text-white transition duration-300" href="/">
+          <h1 className="text-2xl text-white">
+            Steez++
+            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+          </h1>
         </Link>
       </li>
-      <ul className="ml-auto flex items-center gap-x-2">
+      <ul className="ml-auto flex items-center gap-x-6">
         <li>
-          <Link className="text-blue-800 hover:text-blue-900 hover:underline" href="/blog">Blog</Link>
+          <Link className="group text-white transition duration-300" href="/blog">
+            Blog
+            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+          </Link>
         </li>
         <li>
-          <Link className="text-blue-800 hover:text-blue-900 hover:underline" href="/about">About</Link>
+          <Link className="group text-white transition duration-300" href="/about">
+            About
+            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+          </Link>
         </li>
       </ul>
     </ul>
