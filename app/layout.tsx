772f69e1a7c909c939808efbbf3d8535d81eb311
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 16:12:00 -0400

    feat: add css from fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './_styles/globals.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import styles from './_styles/layout.module.css';
 import { Navbar } from '@/app/_components/navbar/Navbar';
 import { Footer } from '@/app/_components/footer/Footer';
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const metadata = {
   title: 'Steez++',
