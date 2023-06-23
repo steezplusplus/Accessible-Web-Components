107aff92f1c55b5b0a44bffddec9ddb87b46dc0b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 14:56:22 -0400

    feat: fix heading element

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ export default function About() {
     <div>
       <header className="bg-gradient-to-r from-blue-800 to-indigo-900">
         <div className="flex flex-col gap-y-2 p-8">
-          <h1 className="text-7xl leading-tight text-white">
+          <h2 className="text-7xl leading-tight text-white">
             About the project
-          </h1>
+          </h2>
           <p className="text-white">In question-answer format</p>
         </div>
       </header>
