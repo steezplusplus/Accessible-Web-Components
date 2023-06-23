bea6702a576b617acf1d2ec892f23032c14b11d1
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 19:47:15 -0400

    feat: simplify article

--- app/blog/_components/article/Article.tsx
+++ app/blog/_components/article/Article.tsx
@@ -9,17 +9,17 @@ export type ArticleProps = {
 export function Article(props: ArticleProps) {
   const { title, description, slug } = props;
   return (
-    <article className="rounded-[0.75rem] border border-gray-500 bg-white p-1">
-      <header className="rounded-lg border-transparent bg-amber-500">
+    <article className="rounded-[0.75rem] border p-1">
+      <header className="rounded-lg px-2 py-1">
         <Link href={`/blog/${slug}`}>
-          <h3 className="px-2 py-1 text-2xl font-bold tracking-tight text-white">{title}</h3>
+          <h3 className="text-2xl font-bold tracking-tight">{title}</h3>
         </Link>
       </header>
       <section className="px-2 py-1">
-        <p className="mb-3 font-normal text-gray-700">{description}</p>
+        <p className="mb-3 font-light text-gray-700">{description}</p>
       </section>
       <footer className="px-2 py-1">
-        <Link className="inline-flex items-center rounded-lg bg-amber-500 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300" href={`/blog/${slug}`}>
+        <Link className="rounded-lg border px-3 text-center text-sm font-medium" href={`/blog/${slug}`}>
           Read more
         </Link>
       </footer>
