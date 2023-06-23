2ff7825516ec5a1e5866743bcc6d8d81f9315754
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-22 23:57:57 -0400

    feat: simple, professional navbar

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -2,26 +2,26 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <ul className="flex h-full items-center bg-blue-500 px-5">
+    <ul className="flex h-full items-center border-b border-solid bg-white px-8">
       <li>
-        <Link className="group text-white transition duration-300" href="/">
-          <h1 className="text-2xl text-white">
+        <Link className="group text-black transition duration-300" href="/">
+          <h1 className="text-2xl text-black">
             Steez++
-            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+            <span className="block h-0.5 max-w-0 bg-black transition-all duration-700 group-hover:max-w-full" />
           </h1>
         </Link>
       </li>
       <ul className="ml-auto flex items-center gap-x-6">
         <li>
-          <Link className="group text-white transition duration-300" href="/blog">
-            Blog
-            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+          <Link className="group text-black transition duration-300" href="/about">
+            About
+            <span className="block h-0.5 max-w-0 bg-black transition-all duration-700 group-hover:max-w-full" />
           </Link>
         </li>
         <li>
-          <Link className="group text-white transition duration-300" href="/about">
-            About
-            <span className="block h-0.5 max-w-0 bg-white transition-all duration-700 group-hover:max-w-full" />
+          <Link className="group text-black transition duration-300" href="/blog">
+            Blog
+            <span className="block h-0.5 max-w-0 bg-black transition-all duration-700 group-hover:max-w-full" />
           </Link>
         </li>
       </ul>
