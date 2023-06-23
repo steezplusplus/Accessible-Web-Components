6f5e4f0d3bea8fcb41527d5fe55c93c396c2fbd6
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-23 15:25:37 -0400

    feat: article different gradients boilerplate

--- app/blog/_components/article/Article.tsx
+++ app/blog/_components/article/Article.tsx
@@ -1,8 +1,25 @@
-export function Article() {
+const gradients = [
+  'bg-gradient-to-r from-indigo-400 to-cyan-400',
+  'bg-gradient-to-r from-amber-200 to-yellow-500',
+  'bg-gradient-to-r from-purple-500 to-purple-900',
+  'bg-gradient-to-r from-cyan-500 to-blue-500',
+  'bg-gradient-to-r from-violet-200 to-pink-200',
+  'bg-gradient-to-r from-fuchsia-500 to-cyan-500',
+  'bg-gradient-to-r from-amber-500 to-pink-500',
+  'bg-gradient-to-r from-fuchsia-600 to-purple-600',
+  'bg-gradient-to-r from-rose-400 to-red-500',
+];
+
+export type ArticleProps = {
+  gradient: number;
+};
+
+export function Article(props: ArticleProps) {
+  const { gradient } = props;
   return (
-    <article className="mb-5 max-w-sm rounded-lg border border-gray-200 bg-white shadow-md">
+    <article className="w-full rounded-lg border border-gray-200 bg-white shadow-md">
       <a href="#">
-        <div className="h-48 w-full bg-amber-500" />
+        <div className={`h-48 w-full ${gradients[gradient]}`} />
       </a>
       <div className="p-5">
         <a href="#">
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -14,16 +14,16 @@ export default async function Blog() {
         </ul>
       </header>
 
-      <section className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
+      <section className="grid gap-10 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <Article gradient={0} />
+        <Article gradient={1} />
+        <Article gradient={2} />
+        <Article gradient={3} />
+        <Article gradient={4} />
+        <Article gradient={5} />
+        <Article gradient={6} />
+        <Article gradient={7} />
+        <Article gradient={8} />
       </section>
 
     </div>
