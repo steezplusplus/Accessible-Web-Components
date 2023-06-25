517854955e1d672cfd45ebf1954fa7efd61a0677
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-24 20:08:10 -0400

    feat: init MDX blogpage

---
+++ app/blog/0/page.tsx
@@ -0,0 +1,5 @@
+import Example from './example.mdx';
+
+export default function Page() {
+  return <Example />;
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    mdxRs: true, // TODO Experimental, maybe bad idea?
+  },
+};
+ 
+const withMDX = require('@next/mdx')({
+  options: {
+    providerImportSource: '@mdx-js/react',
+  }
+});
+module.exports = withMDX(nextConfig);
