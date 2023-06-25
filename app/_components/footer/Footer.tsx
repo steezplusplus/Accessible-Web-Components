7e59255988a2b937cf4bd17b6e6cca5c301aa06d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-25 16:22:30 -0400

    feat: Do some work on the footer

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -1,23 +1,40 @@
-// TODO Add links to linkedin, github. 
-// TODO Add button that scrolls to top.
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const LinkedInURL = new URL('https://www.linkedin.com/in/jesse-bp/');
+const GithubURL = new URL('https://github.com/steezplusplus');
+
 export function Footer() {
   return (
-    <ul className="flex h-full items-center justify-between border border-solid px-1 text-xs">
-      <li>
-        <p>Lorem</p>
-      </li>
-      <li>
-        <p>ipsum</p>
-      </li>
-      <li>
-        <p>dolor</p>
-      </li>
-      <li>
-        <p>sit</p>
-      </li>
-      <li>
-        <p>amet</p>
-      </li>
-    </ul>
+    <div className="flex h-full bg-gradient-to-r from-emerald-500 to-emerald-900">
+      <ul className="flex h-full flex-col p-1 text-xs text-white">
+        <li>
+          <p>
+            &copy; {new Date().getFullYear()} Steez++
+          </p>
+        </li>
+        <li>
+          <p>
+            <i>Supporting resources to make the web accessible</i>
+          </p>
+        </li>
+      </ul>
+
+      <ul className="ml-auto flex items-center gap-x-4 pr-2 text-white">
+        <li>
+          <p className="flex items-center gap-x-1 text-xs hover:underline">
+            <FontAwesomeIcon icon={faLinkedin} />
+            <Link href={LinkedInURL}>LinkedIn</Link>
+          </p>
+        </li>
+        <li>
+          <p className="flex items-center gap-x-1 text-xs hover:underline">
+            <FontAwesomeIcon icon={faGithub} />
+            <Link href={GithubURL}>Github</Link>
+          </p>
+        </li>
+      </ul>
+    </div >
   );
 }
