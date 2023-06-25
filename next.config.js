4f1de8a48a6dd567da72b00f1fe135081ed5521f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-25 18:05:48 -0400

    build: update mdx configs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
-  experimental: {
-    mdxRs: true, // TODO Experimental, maybe bad idea?
-  },
 };
  
-const withMDX = require('@next/mdx')({
-  options: {
-    providerImportSource: '@mdx-js/react',
-  }
-});
+// Merge MDX config with Next.js config
 module.exports = withMDX(nextConfig);
