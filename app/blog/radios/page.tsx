9c6025766638a393cf3dbf5cbebf713325f529b7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-25 20:20:30 -0400

    refactor: prepare radio page

--- app/blog/radios/page.tsx
+++ app/blog/radios/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
-import Example from './example.mdx';
+import Radio from './radio.mdx';
 
 export default function Page() {
-  return <Example />;
+  return (
+    <>
+      <h1>Radio</h1>
+      <Radio />
+      <fieldset>
+        <label>
+          1
+          <input type="radio" name="name" />
+        </label>
+        <label>
+          2
+          <input type="radio" name="name" />
+        </label>
+        <label>
+          3
+          <input type="radio" name="name" />
+        </label>
+      </fieldset>
+    </>
+  );
 }
