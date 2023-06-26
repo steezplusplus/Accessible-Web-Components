4d38da6397497278aa4ca0413b3188235cfbc9e8
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-26 00:34:04 -0400

    feat: work on tabs

--- app/blog/tabs/page.tsx
+++ app/blog/tabs/page.tsx
@@ -1,12 +1,57 @@
 "use client";
 
-import Tabs from './tabs.mdx';
+import React from 'react';
+
+const tabsProps: TabsProps = {
+  tabId: 'tabId',
+  tabs: [
+    {
+      name: 'tab 1',
+      section: <p>section 1</p>,
+    },
+    {
+      name: 'tab 2',
+      section: <p>section 2</p>,
+    },
+    {
+      name: 'tab 3',
+      section: <p>section 3</p>,
+    },
+  ],
+};
 
 export default function Page() {
   return (
-    <article className="prose mx-auto md:prose-lg lg:prose-xl">
-      <h1>Tabs</h1>
-      <Tabs />
-    </article>
+    <Tabs {...tabsProps} />
+  );
+}
+
+type TabsProps = {
+  tabId: string;
+  tabs: {
+    name: string;
+    section: React.ReactNode;
+  }[];
+};
+
+// TODO SHOW SECTION
+function Tabs(props: TabsProps) {
+  const { tabId, tabs } = props;
+  return (
+    <ul>
+      {
+        tabs.map((tab, i) => {
+          const { name, section } = tab;
+          return (
+            <li key={`${name}-${i}`}>
+              <label>
+                {name}
+                <input type="radio" name={tabId} />
+              </label>
+            </li>
+          );
+        })
+      }
+    </ul>
   );
 }
