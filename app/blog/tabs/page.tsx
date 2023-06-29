8d6290e9cd807c0d0992b93f32efa0f6eda93359
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-29 13:04:55 -0400

    fix: fix more stuff to test deployment to vercel

--- app/blog/tabs/page.tsx
+++ app/blog/tabs/page.tsx
@@ -1,57 +1,13 @@
 'use client';
-
-import React from 'react';
-
-const tabsProps: TabsProps = {
-  tabId: 'tabId',
-  tabs: [
-    {
-      name: 'tab 1',
-      section: <p>section 1</p>,
-    },
-    {
-      name: 'tab 2',
-      section: <p>section 2</p>,
-    },
-    {
-      name: 'tab 3',
-      section: <p>section 3</p>,
-    },
-  ],
-};
-
 export default function Page() {
   return (
-    <Tabs {...tabsProps} />
+    <Tabs />
   );
 }
-
-type TabsProps = {
-  tabId: string;
-  tabs: {
-    name: string;
-    section: React.ReactNode;
-  }[];
-};
-
 // TODO SHOW SECTION
-function Tabs(props: TabsProps) {
-  const { tabId, tabs } = props;
+function Tabs() {
+  // const { tabId, tabs } = props;
   return (
-    <ul>
-      {/* {
-        tabs.map((tab, i) => {
-          const { name, section } = tab;
-          return (
-            <li key={`${name}-${i}`}>
-              <label>
-                {name}
-                <input type="radio" name={tabId} />
-              </label>
-            </li>
-          );
-        })
-      } */}
-    </ul>
+    <h1>placeholder</h1>
   );
 }
