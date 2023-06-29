67abaa76d48e777ccf522922a158741006a5143a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-06-29 13:51:52 -0400

    feat: part 1 of tabs complete

--- app/blog/tabs/page.tsx
+++ app/blog/tabs/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 
 import { Tabs } from './_components/tabs/Tabs';
+import Part1 from './part1.mdx';
 
 export default function Page() {
   return (
     <>
-      <article className="prose mx-auto md:prose-lg lg:prose-xl">
+      <article className="prose mx-auto mt-8 md:prose-lg lg:prose-xl">
         <h1>Tabs</h1>
         <p>
           Many implementations of the Tab exist that do not follow
           proper a11y guidelines. They also tend to have poor UX,
           because they hide content that cannot be linked to.
+          This implementation also assumed that the first tab
+          should be open when the page loads.
         </p>
+        <Part1 />
       </article>
       <Tabs />
     </>
