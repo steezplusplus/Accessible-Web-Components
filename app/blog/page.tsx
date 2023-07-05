1b83465e1a4496f4d91a4b876c7f6d5ee83704a7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-05 16:12:40 -0400

    feat: add short description to blogposts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,18 +8,21 @@ export const metadata = {
 };
 
 export default async function Blog() {
-  const placeholderDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  // const placeholderDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  const tabsDescription = 'Tabs are a horizontal set of controls associated with a panel that will display one panel of content at a time.';
+  const accordionDescrpition = 'Accordions are a vertical set of controls associated with a panel that will display one panel of content at a time.';
+
   return (
     <>
       <Header />
       <section className="grid gap-10 p-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <Article title="Accessible Buttons" description={placeholderDescription} slug="buttons" />
-        <Article title="Accessible Checkboxes" description={placeholderDescription} slug="checkboxes" />
-        <Article title="Accessible Radios" description={placeholderDescription} slug="radios" />
-        <Article title="Accessible Tabs" description={placeholderDescription} slug="tabs" />
-        <Article title="Accessible Modal" description={placeholderDescription} slug="modal" />
-        <Article title="Accessible Alert" description={placeholderDescription} slug="alert" />
-        <Article title="Accessible Accordion" description={placeholderDescription} slug="accordion" />
+        {/* <Article title="Accessible Buttons" description={placeholderDescription} slug="buttons" /> */}
+        {/* <Article title="Accessible Checkboxes" description={placeholderDescription} slug="checkboxes" /> */}
+        {/* <Article title="Accessible Radios" description={placeholderDescription} slug="radios" /> */}
+        <Article title="Tabs" description={tabsDescription} slug="tabs" />
+        {/* <Article title="Accessible Modal" description={placeholderDescription} slug="modal" /> */}
+        {/* // <Article title="Accessible Alert" description={placeholderDescription} slug="alert" /> */}
+        <Article title="Accordion" description={accordionDescrpition} slug="accordion" />
       </section>
       <Pagination />
     </>
