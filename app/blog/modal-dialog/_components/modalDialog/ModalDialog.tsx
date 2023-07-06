0e5166266f4faa2c73965981fbc0b4a989a2bbd7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-06 19:45:51 -0400

    feat: finish modal dialog

--- app/blog/modal-dialog/_components/modalDialog/ModalDialog.tsx
+++ app/blog/modal-dialog/_components/modalDialog/ModalDialog.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRef } from 'react';
 
 const labelId = 'modal-dialog-label';
+const descriptionId = 'modal-dialog-description';
 
 export function ModalDialog() {
   const modaDialogRef = useRef<HTMLDialogElement>(null);
@@ -23,19 +24,21 @@ export function ModalDialog() {
 
   return (
     <>
-      <button onClick={showModalDialog} className="prose rounded border px-2 py-1 text-sm">Open a Modal Dialog...</button>
+      <button onClick={showModalDialog} className="rounded border px-2 py-1 hover:bg-slate-100">Open a Modal Dialog...</button>
       <dialog
-
         ref={modaDialogRef}
         aria-labelledby={labelId}
-        className="prose fixed top-1/4 h-fit rounded border border-black bg-slate-200 p-2 prose-h2:my-1"
+        aria-describedby={descriptionId}
+        autoFocus
+        className="fixed top-1/4 h-fit rounded border border-black bg-slate-200 p-2 "
       >
         <div className="flex justify-center gap-x-4">
-          <h2 id={labelId}>This is a modal dialog!</h2>
-          <button onClick={closeModalDialog}>
+          <h2 id={labelId} className="text-xl font-semibold">Hello!</h2>
+          <button onClick={closeModalDialog} className="ml-auto">
             <FontAwesomeIcon icon={faCircleXmark} />
           </button>
         </div>
+        <p id={descriptionId} className="font-light">This is a modal dialog!</p>
       </dialog>
     </>
   );
