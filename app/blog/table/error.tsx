b899c13e74171defd40b962519bd871afab1d189
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-09 19:27:32 -0400

    feat: init loading and error

---
+++ app/blog/table/error.tsx
@@ -0,0 +1,20 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  // reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <h2>Something went wrong!</h2>
+  );
+}
