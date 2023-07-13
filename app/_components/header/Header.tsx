2acd0ffc07bdb6223c1a546632b26378f30de4bf
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-13 14:46:10 -0400

    feat: better homepage styling

--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -2,7 +2,7 @@ import { ArrowLink } from '../arrowLink/ArrowLink';
 
 export function Header() {
   return (
-    <header className="mb-5 bg-gradient-to-r from-emerald-900 to-emerald-500">
+    <header className="bg-gradient-to-r from-emerald-900 to-emerald-500">
       <ul className="flex flex-col gap-y-8 p-8">
         <li>
           <h2 className="text-7xl leading-tight text-white">Making the web a better place through accessible software.</h2>
