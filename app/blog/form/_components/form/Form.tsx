472d5359e2b91dc6e1be750eac1e6c85264659ea
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-15 19:08:39 -0400

    feat: idk

--- app/blog/form/_components/form/Form.tsx
+++ app/blog/form/_components/form/Form.tsx
@@ -1,4 +1,5 @@
 
+// TODO Add reveal password button
 export function Form() {
   return (
     <form className="flex flex-col gap-y-2">
@@ -6,14 +7,14 @@ export function Form() {
         <label htmlFor="username">
           Username
         </label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" name="username"></input>
       </div>
       <div className="flex flex-col gap-y-0.5">
         <label htmlFor="password">
           <div>Password</div>
           <div className="text-xs" >Must contain 8+ characters with at least 1 number and 1 uppercase letter.</div>
         </label>
-        <input id="password" type="password"></input>
+        <input id="password" type="password" name="password"></input>
       </div>
       <button
         className="rounded border bg-emerald-600 px-4 py-2 text-white hover:bg-emerald-700"
