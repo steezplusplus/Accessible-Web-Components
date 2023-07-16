79be36f75a6aa3ff85042cf3592c9efcc9bfb845
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-07-16 10:36:29 -0400

    feat: add show password button

--- app/blog/form/_components/form/Form.tsx
+++ app/blog/form/_components/form/Form.tsx
@@ -1,26 +1,54 @@
+'use client';
 
-// TODO Add reveal password button by swapping type="password" with type="text", toggle aria-pressed and styling
+import { useState } from 'react';
+
+// TODO Style pressed state of button
 export function Form() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
-    <form className="flex flex-col gap-y-2">
+    <form className="flex flex-col gap-y-3">
+
       <div className="flex flex-col gap-y-0.5">
         <label htmlFor="email">
           Email
         </label>
         <input id="email" type="email" name="email"></input>
       </div>
+
       <div className="flex flex-col gap-y-0.5">
         <label htmlFor="password">
           <div>Choose password</div>
           <div className="text-xs" >Must contain 8+ characters with at least 1 number and 1 uppercase letter.</div>
         </label>
-        <input id="password" type="password" name="password"></input>
+        <div className="flex">
+          <input
+            id="password"
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            className="flex-1"
+          />
+          <button
+            type="button"
+            aria-pressed={showPassword}
+            onClick={handleShowPassword}
+            className="bg-emerald-600 px-2 py-1 text-white hover:bg-emerald-700"
+          >
+            Show Password
+          </button>
+        </div>
       </div>
+
       <button
         className="rounded border bg-emerald-600 px-4 py-2 text-white hover:bg-emerald-700"
       >
         Submit
       </button>
+
     </form>
   );
 }
